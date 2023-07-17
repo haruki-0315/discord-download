a51c1a71c3e6137b36790379ce50a26fccc06e29
haruki-0315 <136694981+haruki-0315@users.noreply.github.com>
2023-07-17 15:16:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function handleLogin(event) {
     
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-
-const secretKey = "discord_download_login";
     
     // JSONデータの読み込み（サーバー上にjsonファイルを配置してください）
     fetch("users.json")
@@ -13,11 +11,12 @@ const secretKey = "discord_download_login";
         var found = false;
         for (var i = 0; i < data.length; i++) {
           if (data[i].username === username && data[i].password === password) {
+              sessionStorage.setItem("secretKey", "discord_download_login");
             found = true;
-              req.session.secretKey = secretKey;
             break;
           }
         }
+
         if (found) {
           window.location.href = "download.html"; // ダウンロードページにリダイレクト
         } else {
@@ -27,13 +26,3 @@ const secretKey = "discord_download_login";
       })
       .catch(error => console.log(error));
   }
-
-app.get("download.html", (req,res) => {
-    const sessionKey = req.session.secretKey;
-    if (sessionKey === secretKey) {
-        res.send("アクセスが許可されました");
-
-    } else {
-        res.redirect("index.html");
-    }
-});
