671eee32fefc254b11fdbb00f0ee06dabe6cdfff
はるっきー <136694981+haruki-0315@users.noreply.github.com>
2023-07-29 14:53:32 +0900

    Create form.php

---
+++ php/form.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // ここでフォームのデータを処理するためのコードを追加します
+  
+  // 例として、フォームのデータをメールで送信するコードを追加します
+  $to = "your-email@example.com"; // 宛先メールアドレスを指定します
+  $subject = "お問い合わせフォームからのメッセージ";
+  $body = "お名前: $name\nメールアドレス: $email\nメッセージ: $message";
+
+  if (mail($to, $subject, $body)) {
+    echo "メッセージが送信されました。";
+  } else {
+    echo "メッセージの送信に失敗しました。";
+  }
+}
+?>
