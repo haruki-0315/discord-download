0c56ceec27e03f8e311f426624ec3981bd600f7e
はるっきー <136694981+haruki-0315@users.noreply.github.com>
2023-08-11 13:51:54 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-function memberlogin() {
+function pjsk_memberlogin() {
     window.location.href = "pjsk-memberlogin.html";
 }
 
-function moderatorlogin() {
-    window.location.href = "moderatorlogin.html";
+function pjsk_moderatorlogin() {
+    window.location.href = "pjsk-moderatorlogin.html";
+}
+
+function yumesute_memberlogin() {
+    window.location.href = "yumesute-memberlogin.html";
+}
+
+function yumesute_moderatorlogin() {
+    window.location.href = "yumesute-moderatorlogin.html";
 }
